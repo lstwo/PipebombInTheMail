5759d285217be094d31a73856994414940b5cbd0
LeShaDDoW <leshaddow3@gmail.com>
2024-09-07 13:25:58 +0200

    changed version number

--- src/main/java/net/lstwo/pipe_bomb_in_the_mail/item/pipebomb/PipeBombItem.java
+++ src/main/java/net/lstwo/pipe_bomb_in_the_mail/item/pipebomb/PipeBombItem.java
@@ -1,10 +1,12 @@
 package net.lstwo.pipe_bomb_in_the_mail.item.pipebomb;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.lstwo.pipe_bomb_in_the_mail.PipeBombInTheMail;
 import net.minecraft.entity.Entity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
+import net.minecraft.world.explosion.Explosion;
 
 public class PipeBombItem extends Item {
 
@@ -30,25 +32,29 @@ public class PipeBombItem extends Item {
 
     public void doExplosion(World world, Entity entity) {
          if (!world.isClient) {
-             world.createExplosion(null, entity.getX(), entity.getY(), entity.getZ(), getExplosionPower(), World.ExplosionSourceType.MOB);
+             world.createExplosion(null, entity.getX(), entity.getY(), entity.getZ(), getExplosionPower(),
+                     world.getGameRules().getBoolean(PipeBombInTheMail.DO_PROPERTY_DAMAGE) ? World.ExplosionSourceType.MOB : World.ExplosionSourceType.NONE);
          }
     }
 
     public void doExplosion(World world, float x, float y, float z) {
         if (!world.isClient) {
-            world.createExplosion(null, x, y, z, getExplosionPower(), World.ExplosionSourceType.MOB);
+            world.createExplosion(null, x, y, z, getExplosionPower(),
+                    world.getGameRules().getBoolean(PipeBombInTheMail.DO_PROPERTY_DAMAGE) ? World.ExplosionSourceType.MOB : World.ExplosionSourceType.NONE);
         }
     }
 
     public void doCustomExplosion(World world, float x, float y, float z, float power) {
         if (!world.isClient) {
-            world.createExplosion(null, x, y, z, power, World.ExplosionSourceType.MOB);
+            world.createExplosion(null, x, y, z, power,
+                    world.getGameRules().getBoolean(PipeBombInTheMail.DO_PROPERTY_DAMAGE) ? World.ExplosionSourceType.MOB : World.ExplosionSourceType.NONE);
         }
     }
 
     public void doMultipliedExplosion(World world, float x, float y, float z, float multiplier) {
         if (!world.isClient) {
-            world.createExplosion(null, x, y, z, multiplier, World.ExplosionSourceType.MOB);
+            world.createExplosion(null, x, y, z, multiplier,
+                    world.getGameRules().getBoolean(PipeBombInTheMail.DO_PROPERTY_DAMAGE) ? World.ExplosionSourceType.MOB : World.ExplosionSourceType.NONE);
         }
     }
 
